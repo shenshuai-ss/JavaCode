59bb91721f6f3c2c65fa21dff2bc7a48b9acb52e
shenshuai-ss <1298737356@qq.com>
2021-01-08 00:18:00 +0800

    Person类

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private int id;
+    private String name;
+    Person(int id,String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId (int id) {
+        this.id = id;
+    }
+
+}
